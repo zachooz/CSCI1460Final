db94d617c98d0ba3e99e605a41e98496cc2b609c
Zachary Hoffman <hoffking@gmail.com>
2020-05-04 19:11:13 -0400

    merge problems

--- main.py
+++ main.py
@@ -6,42 +6,235 @@ from torch.nn import functional as F
 import torch
 import argparse
 import numpy as np
+from preprocess import SummaryDataset
 from tqdm import tqdm  # optional progress bar
+from transformers import GPT2Tokenizer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+# TODO: If we want more than a batch size of 1, we need to pad whole documents, so each document is the same size
+# bc sizes of dataloader tensors must match except in dimension 0
 hyperparams = {
     "rnn_size": 128,
     "embedding_size": 512,
-    "num_epochs": 5,
-    "batch_size": 20,
-    "learning_rate": 0.0001
+    "num_epochs": 1,
+    "batch_size": 1,
+    "learning_rate": 0.0005,
+    "max_sentence_size": 75,
 }
 
+# Get summary for ith example in a dataset
+def generate_summary(idx, dataset, model, tokenizer):
+    batch = dataset[idx]
+
+    paragraph_length = batch["paragraph_length"]
+    sentence_length = batch["sentence_length"]
+    x = batch["document"]
+    paragraph_lengths = torch.LongTensor(np.array([paragraph_length])[None,:]).to(device)
+    sentence_lengths = torch.LongTensor(np.array(sentence_length)[None,:]).to(device)
+    x = torch.LongTensor(np.array(x)[None,:]).to(device)
+
+    y_pred = torch.squeeze(model(x, paragraph_lengths, sentence_lengths))
+
+    print("ORIGINAL:\n")
+    for i in range(paragraph_length):
+        sentence = x[0,i]
+        print(tokenizer.decode(sentence[0:sentence_length[i]].detach().cpu().numpy()) + "\n")
+
+    print("-----------------------------------------------")
+    print("SUMMARY:\n")
+    for i in range(paragraph_length):
+        sentence = x[0,i]
+        if (y_pred[i] > 0.5):
+            print(tokenizer.decode(sentence[0:sentence_length[i]].detach().cpu().numpy()) + " [CONFIDENCE: {}]\n".format(y_pred[i]))
+
+
+
+
+def train(model, train_loader, hyperparams):
+    loss_fn = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), hyperparams['learning_rate'])
+
+    model = model.train()
+    for epoch in range(hyperparams['num_epochs']):
+        for batch in tqdm(train_loader):
+            paragraph_length = batch["paragraph_length"]
+            sentence_length = batch["sentence_length"]
+            x = batch["document"]
+            y = batch['labels']
+
+            paragraph_length = paragraph_length.to(device)
+            sentence_length = sentence_length.to(device)
+            x = x.to(device)
+            y = y.to(device)
+
+            optimizer.zero_grad()
+            y_pred = model(x, paragraph_length, sentence_length)
+            loss = loss_fn(y_pred.float(), y.float())
+
+            loss.backward()  # calculate gradients
+            optimizer.step()  # update model weights
+
+            perplexity = torch.exp(loss).item()
+            print("loss:", loss.item())
+
+        torch.save(model.state_dict(), './model.pt')
+
+def test(model, test_loader):
+    model = model.eval()
+    loss_fn = nn.BCELoss()
+
+    for batch in tqdm(test_loader):
+        paragraph_length = batch["paragraph_length"]
+        sentence_length = batch["sentence_length"]
+        x = batch["document"]
+        y = batch['labels']
+
+        paragraph_length = paragraph_length.to(device)
+        sentence_length = sentence_length.to(device)
+        x = x.to(device)
+        y = y.to(device)
+
+        y_pred = model(x, paragraph_length, sentence_length)
+        loss = loss_fn(y_pred.float(), y.float())
+        print("loss", loss.item())
+
+
+def validate(model, test_loader):
+    model = model.eval()
+    loss_fn = nn.BCELoss()
+
+    for batch in tqdm(test_loader):
+        paragraph_length = batch["paragraph_length"]
+        sentence_length = batch["sentence_length"]
+        x = batch["document"]
+        y = batch['labels']
+
+        paragraph_length = paragraph_length.to(device)
+        sentence_length = sentence_length.to(device)
+        x = x.to(device)
+        y = y.to(device)
+
+        y_pred = model(x, paragraph_length, sentence_length)
+        loss = loss_fn(y_pred.float(), y.float())
+
+        print("loss", loss.item())
+
+# python main.py -l -T data/dailymail/validate
+# python main.py -l -t data/dailymail/test
 if __name__ == "__main__":
-   
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--load", action="store_true",
+                        help="load model.pt")
+    parser.add_argument("-s", "--save", action="store_true",
+                        help="save model.pt")
+    parser.add_argument("-T", "--train", action="store",
+                        help="run training loop")
+    parser.add_argument("-V", "--validate", action="store",
+                        help="run training loop")
+    parser.add_argument("-t", "--test", action="store",
+                        help="run testing loop")
+    parser.add_argument("-S", "--summary", action="store_true",
+                        help="generate summaries")
+    args = parser.parse_args()
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+    print("GATHERING DATA")
+    train_dataset = None
+    if args.train:
+        train_dataset = SummaryDataset(
+            input_folder=args.train,
+            tokenizer=tokenizer,
+            max_sentence_size=hyperparams['max_sentence_size']
+        )
+
+    validate_dataset = None
+    if args.validate:
+        validate_dataset = SummaryDataset(
+            input_folder=args.validate,
+            tokenizer=tokenizer,
+            max_sentence_size=hyperparams['max_sentence_size']
+        )
+
+    test_dataset = None
+    if args.test:
+        test_dataset = SummaryDataset(
+            input_folder=args.test,
+            tokenizer=tokenizer,
+            max_sentence_size=hyperparams['max_sentence_size']
+        )
+
+    train_loader = None
+    if args.train:
+        train_loader = DataLoader(
+            train_dataset, batch_size=hyperparams['batch_size'], shuffle=True)
+
+    validate_loader = None
+    if args.validate:
+        train_loader = DataLoader(
+            validate_dataset, batch_size=hyperparams['batch_size'])
+
+    test_loader = None
+    if args.test:
+        test_loader = DataLoader(test_dataset, batch_size=hyperparams['batch_size'])
+
+    model = SummaryModel(
+        tokenizer.vocab_size + 1,
+        hyperparams['rnn_size'],
+        hyperparams['rnn_size'],
+        hyperparams['embedding_size'],
+        hyperparams['max_sentence_size']
+    ).to(device)
+
+
+
+    if args.load:
+        print("loading saved model...")
+        model.load_state_dict(torch.load('./model.pt', map_location=device))
+
+    if args.summary:
+        generate_summary(0, test_dataset, model, tokenizer)
+
+    if args.train:
+        print("running training loop...")
+        train(model, train_loader, hyperparams)
+    if args.save:
+        print("saving model...")
+        torch.save(model.state_dict(), './model.pt')
+
+    if args.validate:
+        print("running training loop...")
+        validate(model, validate_loader)
+
+    if args.test:
+        print("testing models...")
+        test(model, test_loader)
+
+
+
+
+
+
 
     # TODO: Make sure you modify the `.comet.config` file
-    #experiment = Experiment(log_code=False)
-    #experiment.log_parameters(hyperparams)
+    # experiment = Experiment(log_code=False)
+    # experiment.log_parameters(hyperparams)
 
     # TODO: Load dataset
     # Hint: Use random_split to split dataset into train and validate datasets
-    
-    vocab_size = 1000
-    paragraph_size = 30
-
-    model = SummaryModel(
-        vocab_size,
-        128,
-        128,
-        512,
-        30
-    ).to(device)
-
-    input = torch.LongTensor(np.random.randint(0,999,(5,30,10))).to(device)
-    paragraph_lengths = torch.Tensor(np.random.randint(1,30,(5,))).to(device)
-    sentence_lengths = torch.Tensor(np.random.randint(1,10,(5,30))).to(device)
-    output = model(input, paragraph_lengths, sentence_lengths)
-    print(output.shape)
-
+    #
+    # vocab_size = 1000
+    # paragraph_size = 30
+    #
+    #
+    #
+    # input = torch.LongTensor(np.random.randint(0, 999, (5, 100, 10))).to(device)
+    #
+    # # (batch_size,)
+    # paragraph_lengths = torch.Tensor(np.random.randint(1, 100, (5,))).to(device)
+    #
+    # # (batch_size, paragraph_size)
+    # sentence_lengths = torch.Tensor(np.random.randint(1, 10, (5, 100))).to(device)
+    # output = model(input, paragraph_lengths, sentence_lengths)
+    # print(output.shape)
--- model.py
+++ model.py
@@ -1,18 +1,12 @@
 from torch import nn
-from torch.nn import functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch
-import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def reverse(tensor):
-    idx = [i for i in range(tensor.size(0)-1, -1, -1)]
-    idx = torch.LongTensor(idx).to(device)
-    return tensor.index_select(0, idx)
 
 class SummaryModel(nn.Module):
-    def __init__(self, vocab_size, word_rnn_size, sentence_rnn_size, embedding_size, paragraph_size):
+    def __init__(self, vocab_size, word_rnn_size, sentence_rnn_size, embedding_size, max_sentence_size):
         """
         The Model class for SummaRuNNer
 
@@ -20,79 +14,80 @@ class SummaryModel(nn.Module):
         :param  word_rnn_size: The size of hidden cells in the world level GRU
         :param sentence_rnn_size: The size of hidden cells in the sentence level GRU
         :param embedding_size: The dimension of embedding space
+        :max_sentence_size: The maximum sentence size
         """
         super().__init__()
 
         # TODO: initialize the vocab_size, rnn_size, embedding_size
+        self.num_layers = 1
+        self.num_directions = 2
         self.vocab_size = vocab_size
         self.word_rnn_size = word_rnn_size
         self.sentence_rnn_size = sentence_rnn_size
         self.embedding_size = embedding_size
+        self.max_sentence_size = max_sentence_size
 
         # TODO: initialize embeddings, LSTM, and linear layers
         self.embedding = torch.nn.Embedding(vocab_size, self.embedding_size)
-        self.word_gru_reversed = torch.nn.GRU(self.embedding_size, word_rnn_size, num_layers=1)
-        self.word_gru = torch.nn.GRU(self.embedding_size, word_rnn_size, num_layers=1)
-        self.sentence_gru = torch.nn.GRU(self.word_rnn_size, sentence_rnn_size, num_layers=1)
-        self.sentence_gru_reversed = torch.nn.GRU(self.word_rnn_size, sentence_rnn_size, num_layers=1)
+        self.word_gru = torch.nn.GRU(self.embedding_size, word_rnn_size, num_layers=self.num_layers, bidirectional=True, batch_first=True)
+        self.sentence_gru = torch.nn.GRU(
+            self.word_rnn_size * max_sentence_size, sentence_rnn_size,
+            num_layers=self.num_layers,
+            bidirectional=True, batch_first=True
+        )
 
         self.AvgPool1 = torch.nn.AvgPool1d(2)
         self.AvgPool2 = torch.nn.AvgPool1d(2)
 
-        self.linear = torch.nn.Linear(sentence_rnn_size, 2)
+        self.linear = torch.nn.Linear(sentence_rnn_size, 1)
+        self.sigmoid = nn.Sigmoid()
 
+    def word_level_gru(self, paragraph, sentence_lengths):
+        """
+        Given a list of sentences, run the word level bidirectional RNN to compute the sentence level representations
+        for each sentence. The sentence level representation for a sentence is obtained by computing the hidden state
+        representations at each word position sequentially, based on the current word embeddings and the previous
+        hidden state. These hidden states are then concatenated together and average pooled to form the sentence
+        level representation.
 
-    
-
-    """
-    Given a list of sentences, run the word level bidirectional RNN to compute the word level representations
-    for each sentence. The word level representation for a sentence is obtained by first taking the final 
-    hidden states obtained by the forward and backward GRU. These hidden states are then concatenated together 
-    and average pooled
-
-    :param paragraph: A tensor (paragraph_size, sentence_size) of sentences. Each sentence is a list of token ids.
-    :param sentence_lengths: A tensor (paragraph_size,) representing the actual lengths (no padding) of each sentence in the paragraph
-    """
-    def compute_word_level_representation(self, paragraph, sentence_lengths):
+        :param paragraph: A tensor (paragraph_size, sentence_size) of sentences. Each sentence is a list of token ids.
+        :param sentence_lengths: A tensor (paragraph_size,) representing the actual lengths (no padding) of each sentence in the paragraph
+        """
         # Word level GRU applied to each sentence in paragraph
+        paragraph_size = paragraph.size()[0]
+
         embeddings = self.embedding(paragraph) # Should have shape (max paragraph size, max sentence size, embedding size)
-        reversed_embeddings = reverse(embeddings)
-        reversed_lengths = reverse(sentence_lengths)
 
-        packed_seq = pack_padded_sequence(embeddings, sentence_lengths, batch_first=True, enforce_sorted = False)
-        packed_seq_reversed = pack_padded_sequence(reversed_embeddings, reversed_lengths, batch_first=True, enforce_sorted = False)
-        _, out = self.word_gru(packed_seq)
-        _, out_reversed = self.word_gru_reversed(packed_seq_reversed)
+        packed_seq = pack_padded_sequence(embeddings, sentence_lengths, batch_first=True, enforce_sorted=False)
 
-        # Concatenate the two results and average pool them
-        word_level_representations = torch.cat((out, out_reversed), 2)
-        pooled = self.AvgPool1(word_level_representations)
-        return torch.squeeze(pooled)
+        # (paragraph_size, sentences_len, num_directions * hidden_size)
+        word_level_representations, _ = self.word_gru(packed_seq)
+        word_level_representations, lens_unpacked = pad_packed_sequence(
+            word_level_representations,
+            batch_first=True,
+            total_length=self.max_sentence_size
+        )
 
-    """
-    Given a list of word_level_representations, run the sentence level bidirectional RNN to compute the sentence
-    level representations. The sentence level representations for a paragraph are obtained by taking the outputs
-    at each timestep of the sentence level RNNs over the paragraph.
+        # Average pool results
+        word_level_representations = self.AvgPool1(word_level_representations)
 
-    :param hidden_states: A tensor containing paragraphs (batch_size, paragraph_size, word_rnn_size).
-    :param sentence_lengths: A tensor (batch_size,) containing the actual lengths of each sentence in the paragraph
-    """
-    def compute_sentence_level_representation(self, hidden_states, paragraph_lengths):
-        
-        hidden_states_reversed = reverse(hidden_states)
-        reversed_lengths = reverse(paragraph_lengths)
+        # Concatenate results (paragraph_size, sentences_len * hidden_size)
+        # Gives 1 representation per sentence to give to GRU
+        word_level_representations = word_level_representations.view(paragraph_size, -1)
 
-        packed_seq = pack_padded_sequence(hidden_states, paragraph_lengths, batch_first=True, enforce_sorted = False)
-        packed_seq_reversed = pack_padded_sequence(hidden_states_reversed, reversed_lengths, batch_first=True, enforce_sorted = False)
-        out, _ = self.sentence_gru(packed_seq)
-        out_reversed, _ = self.sentence_gru_reversed(packed_seq_reversed)
+        return word_level_representations
 
-        padded_seq, lengths = pad_packed_sequence(out, batch_first=True, total_length=hidden_states.shape[1])
-        padded_seq_reversed, reversed_lengths = pad_packed_sequence(out_reversed, batch_first=True, total_length=hidden_states.shape[1])
+    def sentence_level_gru(self, paragraphs, paragraph_lengths):
+        """
+        Given a list of sentence reperesentations, run the sentence level bidirectional RNN to compute the paragraph
+        level representations.
 
-        # Concatenate the two results and average pool them
-        sentence_representations = torch.cat((padded_seq, padded_seq_reversed), 2)
-        return self.AvgPool1(sentence_representations)
+        :param hidden_states: A tensor containing paragraphs (batch_size, paragraph_size, sentence_representation_size)
+        :param sentence_lengths: A tensor (batch_size,) containing the actual lengths of each sentence in the paragraph
+        """
+        # (batch_size, paragraph_len, num_directions * hidden_size)
+        out, _ = self.sentence_gru(paragraphs)
+        return self.AvgPool1(out)
 
 
 
@@ -110,18 +105,38 @@ class SummaryModel(nn.Module):
                  (batch_size, window_size, vocab_size)
         """
 
-        #TODO: Figure out how to parallelize the model
+        # TODO: Parallelize the model
+        # To parallelize model we need to pass all sentences into a single call word_level_gru
+        # and group their embeddings by paragraph. Then pass that to sentence_level_gru
 
         # Compute word level representations over all sentences
         # Result should have shape (batch_size, paragraph_size, word_rnn_size)
-        word_level_outputs =[self.compute_word_level_representation(inputs[i], sentence_lengths[i]) for i in range(len(inputs))]
+
+        word_level_outputs = [self.word_level_gru(inputs[i], sentence_lengths[i]) for i in range(len(inputs))]
         word_level_outputs = torch.stack(word_level_outputs)
 
+        # Parallelization attempt
+        # inputs = inputs.view(-1, self.max_sentence_size)
+        # sentence_lengths = sentence_lengths.view(-1)
+        # word_level_outputs = self.word_level_gru(inputs, sentence_lengths)
+
+        # have to recombine to paragraphs
+        # index = 0
+        # paragraphs = []
+        # for length in paragraph_lengths:
+        #     paragraphs.append(torch.narrow(word_level_outputs, 0, index, index + length))
+        #     index += length
+        #
+        # word_level_outputs = torch.stack(paragraphs)
+
+        # print(paragraphs.size())
+
         # Run sentence level bidirectional GRU over the resulting hidden states
-        # Should have shape (batch_size, paragraph_size, sentence_rnn_size) 
-        sentence_level_outputs = self.compute_sentence_level_representation(word_level_outputs, paragraph_lengths)
+        # Should have shape (batch_size, paragraph_size, sentence_rnn_size)
+        sentence_level_outputs = self.sentence_level_gru(word_level_outputs, paragraph_lengths)
 
         # TODO: Apply non-linear transformation as described in paper to get representation of document
 
         # Add logistic layer to make binary decision
-        return self.linear(sentence_level_outputs)
+        l1 = self.linear(sentence_level_outputs).squeeze(2)
+        return self.sigmoid(l1)
